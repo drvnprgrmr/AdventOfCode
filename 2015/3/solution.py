971742fdb5c809baa50625fe74169babe44de34f
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2026-05-06 19:43:25 +0100

    2015/3:1 solved

--- 2015/3/solution.py
+++ 2015/3/solution.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from os import path
 from sys import stderr
 
@@ -8,7 +9,8 @@ DAY: int = 3
 
 puzzle = Puzzle(YEAR, DAY)
 
-testing = False
+parser = ArgumentParser("Advent of Code solution file")
+parser.add_argument("-t", "--test", action="store_true")
 
 
 def load_input_data() -> str:
@@ -26,7 +28,9 @@ def load_input_data() -> str:
 def solve_a(raw_data: str) -> int:
     visited_positions = set()
     current_position = [0, 0]  # x, y
-    revisit_count = 0
+    current_position_tuple = tuple(current_position)  # add the starting postion
+
+    visited_positions.add(current_position_tuple)
 
     for dir in data:
         if dir == "^":  # up
@@ -37,18 +41,12 @@ def solve_a(raw_data: str) -> int:
             current_position[0] += 1
         elif dir == "<":  # left
             current_position[0] -= 1
-        else:
-            print(f"unrecognised character: {dir}", file=stderr)
-            exit()
 
         # check if position has been visited previously
         current_position_tuple = (current_position[0], current_position[1])
-        if current_position_tuple in visited_positions:
-            revisit_count += 1
-        else:
-            visited_positions.add(current_position_tuple)
+        visited_positions.add(current_position_tuple)
 
-    return revisit_count
+    return visited_positions.__len__()
 
 
 def solve_b(raw_data: str) -> int:
@@ -58,16 +56,19 @@ def solve_b(raw_data: str) -> int:
 if __name__ == "__main__":
     data = load_input_data()
 
+    # load parser's arguments
+    args = parser.parse_args()
+
     if not puzzle.answered("a"):
         answer_a = solve_a(data)
-        if not testing:
+        if not args.test:
             puzzle.answer_a = answer_a
         else:
             print(answer_a)
 
     elif not puzzle.answered("b"):
         answer_b = solve_b(data)
-        if not testing:
+        if not args.test:
             puzzle.answer_b = answer_b
         else:
             print(answer_b)
