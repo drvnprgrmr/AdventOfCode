091d949085430700e50be8968c5488f48cb54b33
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2026-09-10 00:44:37 +0100

    2015/7:1 solved

--- 2015/7/solution.py
+++ 2015/7/solution.py
@@ -2,12 +2,13 @@ from argparse import ArgumentParser
 from collections import defaultdict
 from enum import Enum
 from os import path
+from pprint import pprint
 
 from aocd.examples import Example
 from aocd.models import Puzzle
 
 YEAR: int = 2015
-DAY: int = 6
+DAY: int = 7
 
 puzzle = Puzzle(YEAR, DAY)
 
@@ -38,18 +39,94 @@ def save_anwer(data: str, file_suffix: str):
 
 # -------------------------------------------------------------
 
-wire_values = defaultdict(lambda: -1)
-input_wires = set()  # wires that have no input from any wires
-output_wires = set()  # wires that don't give output signals to a wire
+wire_values = defaultdict[str, int](lambda: -1)
+input_wires: list[str] = []
 
 
 def solve_a(data: str) -> int:
-    for line in data.splitlines():
-        pass
-    return 0
+    lines = data.splitlines()
+    unprocessed_count = len(lines)
+    processed_lines = [False for _ in range(unprocessed_count)]
+
+    while unprocessed_count:
+        for i, line in enumerate(lines):
+            if processed_lines[i]:  # skip this line if it's been processed already
+                continue
+
+            tokens = line.split()  # split line into tokens for parsing
+            if len(tokens) == 3:  # This is an initial wire assignment
+                left = tokens[0]
+                out = tokens[2]
+
+                if left.isnumeric():
+                    # update the wire
+                    input_wires.append(out)
+                    wire_values[out] = int(left)
+
+                elif wire_values[left] == -1:
+                    continue
+
+                else:
+                    wire_values[out] = wire_values[left]
+
+                # mark line as processed
+                unprocessed_count -= 1
+                processed_lines[i] = True
+
+            elif len(tokens) == 4:  # process the NOT operation
+                left = tokens[1]
+                out = tokens[3]
+                if wire_values[left] == -1:
+                    continue
+
+                # perform operation
+                wire_values[out] = 0xFFFF ^ wire_values[left]
+
+                # mark line as processed
+                unprocessed_count -= 1
+                processed_lines[i] = True
+
+            elif len(tokens) == 5:
+                left = tokens[0]
+                op = tokens[1]
+                right = tokens[2]
+                output = tokens[4]
+
+                # check left input
+                if left.isnumeric():
+                    left = int(left)
+                elif wire_values[left] == -1:
+                    continue
+                else:
+                    left = wire_values[left]
+
+                # check right input
+                if right.isnumeric():
+                    right = int(right)
+                elif wire_values[right] == -1:
+                    continue
+                else:
+                    right = wire_values[right]
+
+                # perform operation
+                if op == "AND":
+                    wire_values[output] = left & right
+                elif op == "OR":
+                    wire_values[output] = left | right
+                elif op == "LSHIFT":
+                    wire_values[output] = left << int(right)
+                elif op == "RSHIFT":
+                    wire_values[output] = left >> int(right)
+
+                # mark line as processed
+                unprocessed_count -= 1
+                processed_lines[i] = True
+
+    pprint(wire_values)
+    return wire_values["a"]
 
 
-# ------------------------------------------------
+# --------------------------------------------------------------
 
 
 def solve_b(data: str) -> int:
